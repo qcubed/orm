3ba6b51a438a95d402e01abc546b5e2dc4cb2808
spekary <spekary@gmail.com>
2017-05-17 10:58:06 -0700

    Fixing qcubed.inc.php

--- install/qcubed.inc.php
+++ install/qcubed.inc.php
@@ -1,11 +1,20 @@
-<?php 
+<?php
 
+/**
+ * This default qcubed.inc.php file loads the correct configuration file. It is the top level qcubed.inc.php file, and many
+ * parts of the framework search for this file, so don't move it. However, if you move the project directory, you should move the
+ * pointer that is here to point to that new location.
+ */
 $configPath = "project/includes/configuration";
 
 if (!defined ('__PREPEND_INCLUDED__')) {	// not already included some other way (like with .htaccess file)
-	if (isset($__CONFIG_ONLY__) && $__CONFIG_ONLY__ == true) {
-		require_once($configPath . '/configuration.inc.php');
-	} else {
-		require_once($configPath . '/prepend.inc.php');
-	}
+    if (isset($__CONFIG_ONLY__) && $__CONFIG_ONLY__ == true) {
+        if (file_exists($configPath . '/configuration.inc.php')) {
+            require_once($configPath . '/configuration.inc.php');
+        }
+    } else {
+        if (file_exists($configPath . '/prepend.inc.php')) {
+            require_once($configPath . '/prepend.inc.php');
+        }
+    }
 }
