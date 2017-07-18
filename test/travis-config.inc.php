a34af09a16db127b31feb4f92e7945b575e44d58
spekary <spekary@gmail.com>
2017-07-18 16:14:10 -0700

    Fixing unit testing

--- test/travis-config.inc.php
+++ test/travis-config.inc.php
@@ -19,6 +19,10 @@ define ('QCUBED_CONFIG_DIR', __WORKING_DIR__ . '/test/travis');
 require(QCUBED_CONFIG_DIR . '/CodegenBase.php');
 require( QCUBED_ORM_TOOLS_DIR . '/codegen.cli.php');
 
+// i18n is not required by the actual ORM library, but it is required by the generated type table files in order to pass unit testing
+require(__WORKING_DIR__ . '/vendor/qcubed/i18n/tools/i18n-app.inc.php'); // Include the translation shortcuts. See the Application for translation setup.
+
+
 
 \QCubed\AutoloaderService::instance()
 	->addClassmapFile(QCUBED_PROJECT_MODEL_GEN_DIR . '/_class_paths.inc.php')
